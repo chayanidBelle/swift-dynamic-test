032346f1398bfdca37dcb8e545259478c85ecc62
chayanid.tha <chayanid.tha@dohome.co.th>
2023-05-22 14:41:14 +0700

    clear warning

--- src/pages/shape/index.tsx
+++ src/pages/shape/index.tsx
@@ -1,5 +1,4 @@
 import { Button, Col, Divider, Row, Select } from 'antd';
-import { wrap } from 'module';
 import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
